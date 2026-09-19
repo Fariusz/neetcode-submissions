0cc9b5b22732454707d002f2d8a55c6d3a3cf327
Radosław Loth <61850108+Fariusz@users.noreply.github.com>
2026-09-19 16:12:09 +0200

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.java
@@ -0,0 +1,58 @@
+class MinStack {
+    Map<Integer, Integer> minValueMap = new HashMap<>();
+    int min;
+    int currentPosition;
+    int size;
+    int[] array;
+
+    public MinStack() {
+        size = 10;
+        array = new int[size];
+        currentPosition = 0;
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if (currentPosition == size) {
+            extendArray();
+        }
+        if (currentPosition == 0) {
+            min = val;
+            minValueMap.put(currentPosition, min);
+        }
+        if (val < min) {
+            min = val;
+        }
+
+        array[currentPosition] = val;
+        currentPosition++;
+        minValueMap.put(currentPosition, min);
+    }
+
+    public void pop() {
+        if (currentPosition > 0) {
+            currentPosition--;
+            min = minValueMap.get(currentPosition);
+        }
+    }
+
+    public int top() {
+        return array[currentPosition - 1];
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    private void extendArray() {
+        int oldSize = size;
+        size += 10;
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < oldSize; i++) {
+            newArray[i] = array[i];
+        }
+
+        array = newArray;
+    }
+}
