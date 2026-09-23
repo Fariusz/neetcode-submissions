f49f17a2cda355a740c0090b2b9a8a54f191ffda
Radosław Loth <61850108+Fariusz@users.noreply.github.com>
2026-09-23 21:20:12 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lPointer = 0;
+        int rPointer = 1;
+
+        int maxPrice = 0;
+
+        while (rPointer < prices.length) {
+            if (prices[lPointer] <= prices[rPointer]) {
+                int diff = prices[rPointer] - prices[lPointer];
+                if (diff > maxPrice) {
+                    maxPrice = diff;
+                }
+                rPointer++;
+            } else if (prices[lPointer] > prices[rPointer]) {
+                lPointer = rPointer;
+                rPointer++;
+            }
+            else return 0;
+        }
+        return maxPrice;
+    }
+}
